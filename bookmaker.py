f924a7af68cd55b1ddb7c63bf4a3a6632f82155c
Aleksey Agafonov <lagerfeed05@bk.ru>
2023-10-23 22:57:48 +0300

    implemented bookmaker

--- bookmaker.py
+++ bookmaker.py
@@ -3,9 +3,24 @@ import telebot
 bot = telebot.TeleBot('6961677992:AAEJIq_-hrYOH6b0u1EXwZirjUt9On8EkCc')
 
 
-@bot.message_handler(commands=['start', 'events'])
+@bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, 'hello')
+
+
+@bot.message_handler(commands=['events last week'])
+def main(message):
+    bot.send_message(message.chat.id, '')
+
+
+@bot.message_handler(commands=['events todays'])
+def main(message):
+    bot.send_message(message.chat.id, '')
+
+
+@bot.message_handler(commands=['events next week'])
+def main(message):
+    bot.send_message(message.chat.id, '')
 
 
 bot.infinity_polling()
